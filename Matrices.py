6c101f8ef7dcba27d44bc332d0c7740d399171fa
alejomf <45019743+alejomf@users.noreply.github.com>
2020-03-05 18:56:27 -0500

    archivo .py + descripción del problema + librería
    archivo con la descripción del problema a resolver, incluye librería que recomiendo para realizar la solución.

---
+++ Matrices.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 20:09:21 2020
+@author: s112e10
+"""
+
+'''
+una funcion que reciba dos matrices de tamaño diferente n*m y n*x, 
+llenarla con numeros aleatorios,los tamaños se le piden al usuario, 
+se les hace el producto punto,cambiar por -1 o 0 los numeros primos
+'''
+import numpy as np
+ 
